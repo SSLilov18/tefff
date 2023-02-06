d9535ecda69af39d4096abc752a1b3b79b9a562e
Стивън Лилов <SSLilov18@codingburgas.bg>
2023-02-06 13:27:24 +0200

    Initial commit

---
+++ src/Medicine.java
@@ -0,0 +1,34 @@
+public class Medicine {
+    private String name;
+    private double price;
+    private String expirationDate;
+    public Medicine (){
+        this.name = " ";
+        this.price = 0.0;
+        this.expirationDate = "";    }
+    public Medicine(String name, double price, String expirationDate)
+    {        this.name = name;
+        this.price = price;
+        this.expirationDate = expirationDate;
+    }    public String getName() {
+        return name;    }
+    public void setName(String name) {
+        this.name = name;    }
+    public double getPrice() {
+        return price;    }
+    public void setPrice(double price) {
+        this.price = price;    }
+    public String getExpirationDate() {
+        return expirationDate;    }
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+    @Override
+    public String toString() {
+        return "Medicine{" +
+                "name='" + name + '\'' +
+                ", price=" + price + ", expirationDate='" + expirationDate + '\''
+                +
+                '}';
+    }
+}
---
+++ src/Provider.java
@@ -0,0 +1,40 @@
+public class Provider {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    public Provider(){
+        this.name="Spas";
+        this.address="Slaveikov";
+        this.phoneNumber= "0886661717";
+    }    public Provider(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public String toString() {
+        return "Provider{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
